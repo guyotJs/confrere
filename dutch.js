ee7021641869bb8bcfd123da9294839317a9f2b4
Weston <classicmclive@gmail.com>
2025-04-21 15:44:27 -0400

    Create dutch.js

---
+++ dutch.js
@@ -0,0 +1,12 @@
+let german = JSON.parse(`
+[
+  {
+    "g":"Ik ga weg, -- je het niet erg vindt",
+    "e":"I'm leaving, if you don't mind.",
+    "a":["als",true]
+    "b":["ob",false]
+    "c":["naar",false]
+    "d":["gaan",false]
+  }
+]
+`);
