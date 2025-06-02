8ddeb74a437739ae61db915df38e660795eb8741
Weston <classicmclive@gmail.com>
2025-06-02 15:50:43 -0400

    Update catalan.js

--- catalan/catalan.js
+++ catalan/catalan.js
@@ -79,7 +79,87 @@ let german = JSON.parse(`
     "b": ["nedaven", false],
     "c": ["neden", false],
     "d": ["nedaran", false]
-  }
+  },
+  {
+    "g": "Al principi, ___ va crear el cel i la terra.",
+    "e": "In the beginning, God created the heavens and the earth.",
+    "a": ["Deo", false],
+    "b": ["Dei", false],
+    "c": ["Dios", false],
+    "d": ["Déu", true]
+  },
+  {
+    "g": "Per ___ estudies català?",
+    "e": "Why are you studying Catalan?",
+    "a": ["quoi", false],
+    "b": ["qei", false],
+    "c": ["cuando", false],
+    "d": ["què", true]
+  },
+  {
+    "g": "Teniu alguna ___ disponible?",
+    "e": "Do you have any rooms available?",
+    "a": ["habitació", true],
+    "b": ["espesce", false],
+    "c": ["lloc", false],
+    "d": ["avió", false]
+  },
+  {
+    "g": "Quant costa ___ habitació per a una persona?",
+    "e": "How much does a room for one person cost?",
+    "a": ["um", false],
+    "b": ["una", true],
+    "c": ["eles", false],
+    "d": ["el", false]
+  },
+  {
+    "g": "Si us ___, netegeu la meva habitació.",
+    "e": "Please clean my room.",
+    "a": ["plau", true],
+    "b": ["palau", false],
+    "c": ["macau", false],
+    "d": ["plai", false]
+  },
+  {
+    "g": "Vull un ___ que tingui formatge.",
+    "e": "I want a plate that has cheese.",
+    "a": ["cullera", false],
+    "b": ["plat", true],
+    "c": ["tassa", false],
+    "d": ["escriure", false]
+  },
+  {
+    "g": "___ costa això?",
+    "e": "How much does this costÞ",
+    "a": ["Que", false],
+    "b": ["Quand", false],
+    "c": ["Quo", false],
+    "d": ["Quant", true]
+  },
+  {
+    "g": "No he ___ res de dolent.",
+    "e": "I haven't done anything wrong.",
+    "a": ["faré", false],
+    "b": ["fer", false],
+    "c": ["fet", true],
+    "d": ["haurà", false]
+  },
+  {
+    "g": "Vull ___ amb un advocat.",
+    "e": "I want to speak to a lawyer.",
+    "a": ["parlo", false],
+    "b": ["paralamos", false],
+    "c": ["falan", false],
+    "d": ["parlar", true]
+  },
+  {
+    "g": "Puc simplement pagar una ___ ara?",
+    "e": "Can I just pay a fine now?",
+    "a": ["pot", false],
+    "b": ["multa", true],
+    "c": ["hola", false],
+    "d": ["tres bé", false]
+  },
 ]
 
 `);
