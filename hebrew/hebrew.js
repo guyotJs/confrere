89bf9e78c6e8a54c6f5f30f887dfb37caa34abb8
Weston <classicmclive@gmail.com>
2025-07-05 08:47:18 -0400

    Update hebrew.js

--- hebrew/hebrew.js
+++ hebrew/hebrew.js
@@ -1,9 +1,13 @@
 let german = JSON.parse(`
 [
     {
-        "g":"",
-        "e":""
-    }
+    "g": "Ela ___ ao supermercado onte.",
+    "e": "She went to the supermarket yesterday.",
+    "a": ["iría", false],
+    "b": ["vai", false],
+    "c": ["irá", false],
+    "d": ["foi", true]
+  },
 ]
 `);
 german.sort(() => Math.random() - 0.5);
